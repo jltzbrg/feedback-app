d9da772d0840028543eb9478946f2e9d5e799685
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-01-29 00:28:13 +0100

    Create header component and index.css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <h1>App Content</h1>
+      </div>
+    </>
+  );
+}
