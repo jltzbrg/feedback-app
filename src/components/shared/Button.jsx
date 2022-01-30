8e9efcc96cc148ecf1179255454da1677760e707
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-01-31 00:28:55 +0100

    Creating custom button component

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+function Button({ children, version, type, isDisabled }) {
+  return (
+    <button
+      className={`btn btn-${version}`}
+      type={type}
+      isDisabled={isDisabled}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  version: 'primary',
+  type: 'button',
+  isDisabled: false,
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  version: PropTypes.string,
+  type: PropTypes.string,
+  isDisabled: PropTypes.bool,
+};
+
+export default Button;
