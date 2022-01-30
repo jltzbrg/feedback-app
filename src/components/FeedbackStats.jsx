e1f135bd79dc5db8d8bf61af567157f5c3c2ddca
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-01-30 23:58:50 +0100

    Create average function for calc the rating

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,5 +1,17 @@
-function FeedbackStats() {
-  return <div>Stats</div>;
+function FeedbackStats({ feedback }) {
+  //Durchschnitt berechnen
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+
+  console.log(average);
+  return (
+    <div className='feedback-stats'>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
 }
 
 export default FeedbackStats;
