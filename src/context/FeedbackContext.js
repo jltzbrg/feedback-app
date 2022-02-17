dd8ee62b06f3408fbaded2ae1e153473c3d9f2a8
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-02-18 00:05:38 +0100

    Add edit feedback event handler

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -12,18 +12,34 @@ export const FeedbackProvider = ({ children }) => {
     { id: 5, text: 'This item 5', rating: 5 },
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
+
+  //Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
 
+  //Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
+
+  //Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
+  }
   return (
-    <FeedbackContext.Provider value={{ feedback, addFeedback, deleteFeedback }}>
+    <FeedbackContext.Provider
+      value={{ feedback, addFeedback, deleteFeedback, editFeedback }}>
       {children}
     </FeedbackContext.Provider>
   )
