e51dacaeaff1afa7e2b1e10b6ffcf33774f38fec
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-02-18 00:32:45 +0100

    Add update feedback functionality

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -30,6 +30,15 @@ export const FeedbackProvider = ({ children }) => {
     }
   }
 
+  //Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) =>
+        item.id === id ? { ...id, ...updItem } : { item }
+      )
+    )
+  }
+
   //Set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -41,10 +50,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         addFeedback,
         deleteFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}>
       {children}
     </FeedbackContext.Provider>
